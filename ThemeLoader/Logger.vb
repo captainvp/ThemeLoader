cb54245bea9c39849cca9c1859eebe185b7b2e96
Vladimir Pronchenko <captainvp@yahoo.com>
2024-05-22 17:05:27 -0400

    Add project files.

---
+++ ThemeLoader/Logger.vb
@@ -0,0 +1,16 @@
+﻿Imports System.IO
+Imports System.Windows.Forms
+
+Public Class Logger
+    Public Shared Function LogEntry(ByVal log_msg) As Boolean
+        Try
+            Dim fi As New FileInfo(Application.StartupPath + "\Error.log")
+            Dim tw As StreamWriter = fi.AppendText()
+            tw.WriteLine(DateTime.Now.ToString + ": " + log_msg)
+            tw.Close()
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+End Class
---
+++ ThemeLoader/Module1.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Diagnostics
+
+Module Module1
+
+    Sub Main()
+        Try
+            Dim pr As Process = Process.Start(My.Settings.THEME_FILE)
+            Threading.Thread.Sleep(1000)
+            Try
+                Dim p As Process() = Process.GetProcessesByName("SystemSettings")
+                For i = 0 To p.Length - 1
+                    p(i).Kill()
+                Next
+            Catch ex As Exception
+                Logger.LogEntry(ex.Message + vbCrLf + ex.StackTrace)
+            End Try
+        Catch ex As Exception
+            Logger.LogEntry(ex.Message + vbCrLf + ex.StackTrace)
+        End Try
+
+    End Sub
+
+End Module
